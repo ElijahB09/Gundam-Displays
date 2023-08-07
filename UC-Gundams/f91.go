7ed9f76927634edc0cedf2908361faff9e18e7c3
Elijah Brady <bradyeli09@gmail.com>
2023-08-06 20:43:18 -0500

    Finished rudimentary f91 file

--- UC-Gundams/f91.go
+++ UC-Gundams/f91.go
@@ -1,9 +1,27 @@
 package ucgundams
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/stianeikeland/go-rpio/v4"
 )
 
 func main() {
 	err := rpio.Open()
+	if err != nil {
+		fmt.Println("Could not open: " + err.Error())
+		os.Exit(1)
+	}
+
+	defer rpio.Close()
+
+	pin := rpio.Pin(18)
+
+	pin.Output()
+
+	for i := 0; i < 5; i++ {
+		pin.Low()
+	}
+	pin.High()
 }
