c7aedca0523f3819a8a86907712d0d73d26397a7
Elijah Brady <bradyeli09@gmail.com>
2023-08-08 21:51:20 -0500

    Add in complex logic for recieving and acting on requests

--- src/UC-Gundams/f91.go
+++ src/UC-Gundams/f91.go
@@ -1,27 +1,48 @@
 package ucgundams
 
 import (
+	"errors"
 	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/stianeikeland/go-rpio/v4"
 )
 
 func main() {
+	http.HandleFunc("/f91/on", led_on)
+	http.HandleFunc("/f91/off", led_off)
+
+	err := http.ListenAndServe(":3333", nil)
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	}
+}
+
+func led_on(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("LEDs on\n")
 	err := rpio.Open()
 	if err != nil {
 		fmt.Println("Could not open: " + err.Error())
 		os.Exit(1)
 	}
-
 	defer rpio.Close()
-
-	pin := rpio.Pin(18)
-
+	pin := get_pin()
 	pin.Output()
+	pin.Low()
+}
 
-	for i := 0; i < 5; i++ {
-		pin.Low()
-	}
+func led_off(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("LEDs off\n")
+	pin := get_pin()
 	pin.High()
+	rpio.Close()
+}
+
+func get_pin() rpio.Pin {
+	pin := rpio.Pin(18)
+	return pin
 }
