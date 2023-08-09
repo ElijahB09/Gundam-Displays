04ad455a1a1aaf295a0da0948148bed33b0117b6
ElijahB09 <134438080+ElijahB09@users.noreply.github.com>
2023-08-08 23:09:35 -0500

    Update f91.go with pi success

--- src/UC-Gundams/f91.go
+++ src/UC-Gundams/f91.go
@@ -10,25 +10,20 @@ import (
 	"github.com/stianeikeland/go-rpio/v4"
 )
 
-func led_on(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("LEDs on\n")
-	io.WriteString(w, "Gundam f91 LEDs on\n")
-	pin := get_pin()
-	pin.Output()
-	pin.Low()
+func on(pin rpio.Pin) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("LEDs on\n")
+		io.WriteString(w, "Gundam f91 LEDs on\n")
+		pin.High()
+	}
 }
 
-func led_off(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("LEDs off\n")                   // Goes to console
-	io.WriteString(w, "Gundam f91 LEDs off\n") // Goes to webpage
-	pin := get_pin()
-	pin.High()
-	rpio.Close()
-}
-
-func get_pin() rpio.Pin {
-	pin := rpio.Pin(18)
-	return pin
+func off(pin rpio.Pin) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("LEDs off\n")                   // Goes to console
+		io.WriteString(w, "Gundam f91 LEDs off\n") // Goes to webpage
+		pin.Low()
+	}
 }
 
 func main() {
@@ -38,9 +33,12 @@ func main() {
 		os.Exit(1)
 	}
 	defer rpio.Close()
+	
+	pin := rpio.Pin(18)
+	pin.Output()
 
-	http.HandleFunc("/f91/on", led_on)
-	http.HandleFunc("/f91/off", led_off)
+	http.HandleFunc("/f91/on", on(pin))
+	http.HandleFunc("/f91/off", off(pin))
 
 	err := http.ListenAndServe(":3333", nil)
 	if errors.Is(err, http.ErrServerClosed) {
