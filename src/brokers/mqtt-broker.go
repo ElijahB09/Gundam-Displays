dd30bd91e70a0832c3478dd9f93e8ed0a21f2b14
Elijah Brady <bradyeli09@gmail.com>
2023-08-28 15:26:37 -0500

    Added the mqtt stuff and beginning migration to mqtt from http

---
+++ src/brokers/mqtt-broker.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/joho/godotenv"
+)
+
+var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
+}
+
+var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
+	fmt.Println("Connected")
+}
+
+var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
+	fmt.Printf("Connect lost: %v", err)
+}
+
+func init() {
+	// Load .env variables into system
+	if err := godotenv.Load(); err != nil {
+		fmt.Print("No .env file found")
+	}
+}
+
+func main() {
+	var broker, exists = os.LookupEnv("PI_MQTT_BROKER")
+	if !exists {
+		fmt.Print("Something big gone wrong with .env")
+	}
+	var port = 8083
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(fmt.Sprintf("mqtts://%s:%d", broker, port))
+	opts.SetClientID("go_mqtt_client")
+	opts.SetUsername("emqx")
+	opts.SetPassword("public")
+	opts.SetDefaultPublishHandler(messagePubHandler)
+	opts.OnConnect = connectHandler
+	opts.OnConnectionLost = connectLostHandler
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	sub(client)
+	publish(client)
+
+	client.Disconnect(250)
+}
+
+func publish(client mqtt.Client) {
+	num := 10
+	for i := 0; i < num; i++ {
+		text := fmt.Sprintf("Message %d", i)
+		token := client.Publish("topic/test", 0, false, text)
+		token.Wait()
+		time.Sleep(time.Second)
+	}
+}
+
+func sub(client mqtt.Client) {
+	topic := "topic/test"
+	token := client.Subscribe(topic, 1, nil)
+	token.Wait()
+	fmt.Printf("Subscribed to topic: %s", topic)
+}
