05311c4053eb3d2fbe7ad82325147cf7c7de162a
Elijah Brady <bradyeli09@gmail.com>
2023-09-09 11:42:51 -0500

    Changed it to only take one username and topic

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"strings"
 
 	uc "github.com/ElijahB09/Gundam-Displays/uc"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
@@ -57,26 +56,23 @@ func main() {
 	gundams = append(gundams, Gundam{name: "f91", topic: "gundam/uc/f91/f91gundam"})
 	gundams = append(gundams, Gundam{name: "unicorn", topic: "gundam/uc/unicorn/unicorngundam"})
 
-	for _, element := range gundams {
-		opts := mqtt.NewClientOptions()
-		opts.AddBroker(fmt.Sprintf("mqtt://%s:%d", broker, port))
-		opts.SetClientID("mqtt_client_" + element.name)
-		opts.SetUsername(element.name)
-		password, isThere := os.LookupEnv("GUNDAM_" + strings.ToUpper(element.name) + "_PASSWORD")
-		if !isThere {
-			fmt.Errorf("Something big gone wrong with .env")
-			break
-		}
-		opts.SetPassword(password)
-		opts.SetDefaultPublishHandler(messagePubHandler)
-		opts.OnConnect = connectHandler
-		opts.OnConnectionLost = connectLostHandler
-		client := mqtt.NewClient(opts)
-		if token := client.Connect(); token.Wait() && token.Error() != nil {
-			panic(token.Error())
-		}
-		sub(client, element.topic)
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(fmt.Sprintf("mqtt://%s:%d", broker, port))
+	opts.SetClientID("mqtt_client_" + "f91")
+	opts.SetUsername("f91")
+	password, isThere := os.LookupEnv("GUNDAM_F91_PASSWORD")
+	if !isThere {
+		fmt.Errorf("Something big gone wrong with .env")
 	}
+	opts.SetPassword(password)
+	opts.SetDefaultPublishHandler(messagePubHandler)
+	opts.OnConnect = connectHandler
+	opts.OnConnectionLost = connectLostHandler
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+	sub(client, "gundam/uc/f91/f91gundam")
 }
 
 func sub(client mqtt.Client, mqtt_topic string) {
